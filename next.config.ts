dbc03c301e572fba4d3af502750ca1ab263f44b0
connorknoetze <connorknoetze@email.com>
2025-07-03 21:48:32 +1200

    modified:   app/page.tsx 	modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  reactStrictMode: true,
   images: { unoptimized: true }
 };
 module.exports = nextConfig;
