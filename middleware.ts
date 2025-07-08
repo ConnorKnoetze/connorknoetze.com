709a6a4fe14fb49246bd5af3e0fb72fd11c246bf
connorknoetze <connorknoetze@gmail.com>
2025-07-08 13:33:21 +1200

    deleted:    app/api/test-db/route.ts 	deleted:    middleware.ts

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(request: Request) {
-  const url = new URL(request.url);
-  if (url.pathname === '/api/contact') {
-    return NextResponse.next();
-  }
-  return NextResponse.redirect(new URL('/', request.url));
-}
