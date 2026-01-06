f88e71fd681279aa8a39087e76b105f019d25487
connorknoetze <connorknoetze@gmail.com>
2026-01-06 14:01:17 +1300

    blacklisted 'old-portfolio'

--- src/components/Panels/ProjectPanel/ProjectPanel.js
+++ src/components/Panels/ProjectPanel/ProjectPanel.js
@@ -5,7 +5,7 @@ import WindowControls from "../../WindowControls/WindowControls";
 import "@/styles/Panels/ProjectPanel/ProjectPanel.css";
 
 export default function ProjectPanel(){
-    const blacklistedRepos = ["ConnorKnoetze", "Dart_Board", "Java-OpenGL-Triangle-LWJGL", "new-portfolio"];
+    const blacklistedRepos = ["ConnorKnoetze", "Dart_Board", "Java-OpenGL-Triangle-LWJGL", "new-portfolio", "old_portfolio"];
     const [repos, setRepos] = useState([]);
     const [filteredRepos, setFilteredRepos] = useState([]);
     const [loading, setLoading] = useState(true);
