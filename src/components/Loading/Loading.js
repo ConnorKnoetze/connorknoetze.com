a422e7f6d66c8bfedfd2c74a0baceffe11fb34a7
connorknoetze <connorknoetze@gmail.com>
2026-01-13 21:08:41 +1300

    feat(loading) Added blurred background

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -5,16 +5,18 @@ import { useBackgroundLoading } from '@/context/BackgroundLoadingContext';
 export default function Loading() {
   const { isBackgroundLoaded } = useBackgroundLoading();
 
-  if (isBackgroundLoaded) {
-    return null;
-  }
+  // if (isBackgroundLoaded) {
+  //   return null;
+  // }
 
   return (
     <div className="loading-container">
-      <div className="loading-spinner">
-        <div className="spinner"></div>
+      <div className='loading-content'>
+        <div className="loading-spinner">
+          <div className="spinner"></div>
+        </div>
+        <p className="loading-text">Getting Things Ready For You...</p>
       </div>
-      <p className="loading-text">Loading...</p>
     </div>
   );
 }
